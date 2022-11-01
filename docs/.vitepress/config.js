ac5dcf1937f64b50e76986941c77f9b2b9218753
yiwwhl <yiwwhl@qq.com>
2022-11-01 23:49:10 +0800

    chore: project init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitepress";
+
+const sidebar = [
+  {
+    text: "起源",
+    items: [{ text: "前言", link: "/index.md" }],
+  },
+  {
+    text: "Vim 篇",
+    items: [{ text: "Day01 —— 存活下去", link: "/vim/day_01_ surviving.md" }],
+  },
+];
+
+const themeConfig = {
+  logo: "/vim.svg",
+  socialLinks: [
+    { icon: "github", link: "https://github.com/hlyiww/learn-vim" },
+  ],
+  footer: {
+    message: "Released under the MIT License.",
+    copyright: "Copyright © 2022-present Luckept",
+  },
+  sidebar,
+};
+
+export default defineConfig({
+  title: "Learn Vim",
+  themeConfig,
+  base: "/learn-vim/",
+});
